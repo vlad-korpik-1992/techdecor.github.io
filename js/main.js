de2d004fca715d2d8eb16dda83eceabea992e7a7
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2023-08-29 21:52:47 +0300

    The project is complete. The project to the client showed

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ function openMenu(event) {
     listMenu.classList.toggle('menu__list--active');
     btnElement.classList.toggle('menu__btn__element--close');
 }
+const anchors = document.querySelectorAll('.menu__list__link')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+
+    const btnMenu = document.querySelector('.menu__btn');
+    const listMenu = document.querySelector('.menu__list');
+    const btnElement = document.querySelector('.menu__btn__element');
+    btnMenu.classList.remove('menu__btn--close');
+    listMenu.classList.remove('menu__list--active');
+    btnElement.classList.remove('menu__btn__element--close');
+  })
+}
